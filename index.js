022e950c1630c690d394b10adc88eae50a16906a
FatemaAkter11 <fatemaakter4261@gmail.com>
2021-11-25 23:30:02 +0600

    database connected successfully

--- index.js
+++ index.js
@@ -4,7 +4,23 @@ const uri =
 console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
-  const collection = client.db("test").collection("devices");
+  const images = client.db("chobirhaat").collection("images");
   // perform actions on the collection object
-  client.close();
+
+  app.post("/uploadimg", (req, res) => {
+    const newimg = req.body;
+    images.insertOne(newimg);
+  });
+
+  app.get("/gallery", (req, res) => {
+    images.find({}).toArray((err, documents) => {
+      res.send(documents.slice(0, 12));
+    });
+  });
+  //console.log("database connected");
 });
+
+app.get("/", (req, res) => {
+  res.send("Welcome to CHOBIRHAAT");
+});
+
