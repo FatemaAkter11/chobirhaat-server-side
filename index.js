cc58bb477cc4ff7e2e6aff7ea02472036ff70abf
FatemaAkter11 <fatemaakter4261@gmail.com>
2021-11-26 00:58:02 +0600

    image store completed

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
-const { MongoClient } = require('mongodb');
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const { MongoClient, ObjectId } = require("mongodb");
+
+const port = 5000;
+app.use(cors());
+app.use(bodyParser.json());
+
 const uri =
   "mongodb+srv://chobithaatAdmin:chobirHaat123@chobirhaatcluster.epn3u.mongodb.net/chobirhaat?retryWrites=true&w=majority";
-console.log(uri);
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-client.connect(err => {
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+client.connect((err) => {
   const images = client.db("chobirhaat").collection("images");
   // perform actions on the collection object
 
@@ -17,6 +29,93 @@ client.connect(err => {
       res.send(documents.slice(0, 12));
     });
   });
+
+  app.get("/image/:imgid", (req, res) => {
+    images
+      .find({ _id: ObjectId(req.params.imgid) })
+      .toArray((err, documents) => {
+        res.send(documents[0]);
+      });
+  });
+
+  app.get("/category/:categoryid", (req, res) => {
+    images
+      .find({ category: req.params.categoryid })
+      .toArray((err, documents) => {
+        res.send(documents);
+      });
+  });
+
+  app.get("/imagepending", (req, res) => {
+    images.find({ status: "pending" }).toArray((err, documents) => {
+      res.send(documents);
+    });
+  });
+
+  app.patch("/payment/:imageid", (req, res) => {
+    images
+      .updateOne(
+        { _id: ObjectId(req.params.imageid) },
+        { $set: { trxid: req.body.trxid, status: req.body.status } }
+      )
+      .then((result) => res.send(result));
+  });
+
+  app.patch("/views/:imageid", (req, res) => {
+    images
+      .updateOne(
+        { _id: ObjectId(req.params.imageid) },
+        { $set: { views: req.body.views } }
+      )
+      .then((result) => res.send(result));
+  });
+
+  app.patch("/image/:imageid", (req, res) => {
+    images
+      .updateOne(
+        { _id: ObjectId(req.params.imageid) },
+        {
+          $set: {
+            trxid: req.body.trxid,
+            status: req.body.status,
+            buyer: req.body.buyer,
+            buyeremail: req.body.buyeremail,
+          },
+        }
+      )
+      .then((result) => res.send(result));
+  });
+
+  app.patch("/comment/:imageid", (req, res) => {
+    images
+      .updateOne(
+        { _id: ObjectId(req.params.imageid) },
+        { $set: { comments: req.body } }
+      )
+      .then((result) => res.send(result));
+  });
+
+  //searching for images
+  app.get("/search/:keyword", (req, res) => {
+    images.find({ title: req.params.keyword }).toArray((err, documents) => {
+      res.send(documents);
+    });
+  });
+
+  //searching for users uploaded images
+  app.get("/uploadedimages/:email", (req, res) => {
+    images.find({ selleremail: req.params.email }).toArray((err, documents) => {
+      res.send(documents);
+    });
+  });
+
+  //deleting image
+  app.delete("/image/:imgid", (req, res) => {
+    images.deleteOne({ _id: ObjectId(req.params.imgid) }).then((result) => { });
+  });
+
+
+
   //console.log("database connected");
 });
 
@@ -24,3 +123,4 @@ app.get("/", (req, res) => {
   res.send("Welcome to CHOBIRHAAT");
 });
 
+app.listen(process.env.PORT || port);
